dccb612304e4767cd7a85bdb909ef7a5b313acc3
Kevin-Guegan <k.guegan8@laposte.net>
2015-03-30 15:58:15 +0200

    DAY 12.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,11 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),           
             new AppBundle\AppBundle(),
             new Ibw\JobeetBundle\IbwJobeetBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
